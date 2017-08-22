9f3d7a5837dd0163a2a365b8674ec51bca8e0ca4
AjinkyaGhag <31257517+AjinkyaGhag@users.noreply.github.com>
2017-08-22 16:28:04 -0400

    MySite

---
+++ Home.aspx
@@ -0,0 +1,80 @@
+﻿<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Home</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src="Scripts/jquery-3.1.1.min.js"></script>
+    <script src="Scripts/angular.min.js"></script>
+    <script src="Scripts/firebase.js"></script>
+    <script src="Scripts/angularfire.min.js"></script>
+
+    <script src="Scripts/angular-easyfb.js"></script>
+
+    <script src="Scripts/angular-route.min.js"></script>
+
+
+    <script src="Scripts/angular-ui/ui-bootstrap-tpls.min.js"></script>
+    <script src="Scripts/bootstrap.min.js"></script>
+    <script src="Scripts/angular-animate.min.js"></script>
+    <script src="Scripts/App.js"></script>
+
+    <link href="Styles/Content/bootstrap.min.css" rel="stylesheet" />
+    <link href="Styles/Content/font-awesome.min.css" rel="stylesheet" />
+    <link href="Styles/MySiteCSS.css" rel="stylesheet" />
+
+    <script>
+        // Initialize Firebase
+        var config = {
+            apiKey: "AIzaSyBhUWzYmxl9WPEiYlFLMjKOd7W0FPVpz-k",
+            authDomain: "project1-5f708.firebaseapp.com",
+            databaseURL: "https://project1-5f708.firebaseio.com",
+            storageBucket: "project1-5f708.appspot.com",
+            messagingSenderId: "631686253439"
+        };
+        firebase.initializeApp(config);
+    </script>
+
+</head>
+<body ng-app="myApp">
+    <nav class="navbar navbar-inverse">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#">AJ</a>
+            </div>
+            <div class="collapse navbar-collapse" id="myNavbar" ng-controller="NavCtrl">
+                <ul class="nav navbar-nav" >
+                    <li ng-class="navClass('home')"><a href="#">Home</a></li>
+                    <li ng-class="navClass('AboutMe')"><a href="#/AboutMe">About Me</a></li>
+                    <li ng-class="navClass('Contact')"><a href="#/Contact">Contact</a></li>
+                    <li class="dropdown">
+                        <a class="dropdown-toggle" data-toggle="dropdown">Upcoming Projects
+                            <span class="caret"></span>
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li ng-class="navClass('FreeComputerClass')"><a href="#/FreeComputerClass">Free Computer Class/Training/Help</a></li>
+                            <li ng-class="navClass('FoodTruckMumbai')"><a href="#/FoodTruckMumbai">Food Truck Mumbai</a></li>
+                        </ul>
+                    </li>
+                </ul>
+                <ul class="nav navbar-nav navbar-right" ng-controller="SampleCtrl">
+                    <li ng-show="!firebaseUser" ng-class="navClass('Register')"><a href="#/Register"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+                    <li ng-show="!firebaseUser" ng-class="navClass('Login')"><a href="#/Login"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+                    <li ng-show="firebaseUser"><a href="#"><span class="glyphicon glyphicon-user"></span> {{ firebaseUser.displayName }}</a> </li>
+                    | 
+                    <li ng-show="firebaseUser"><a href="#" ng-click="SignOutUSer()"><span class="glyphicon glyphicon-log-out"></span> Sign out</a></li>
+
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <div ng-view></div>
+</body>
+</html>
+
---
+++ Scripts/App.js
@@ -0,0 +1,259 @@
+﻿var app = angular.module("myApp", ["ezfb", "ngRoute", "ngAnimate", "ui.bootstrap","firebase"]);
+
+
+
+
+app.config(function (ezfbProvider,$routeProvider, $locationProvider) {
+    ezfbProvider.setInitParams({
+        appId: '386469651480295',
+        version: 'v2.6'
+    })
+    $locationProvider.hashPrefix('');
+    $routeProvider
+    .when("/", {
+        templateUrl: "/Views/Main.html",
+        controller: "SampleCtrl"
+    })
+    .when("/FoodTruckMumbai", {
+        templateUrl: "/Views/FoodTruckMumbai.html",
+        controller: "FoodTruckMumbaiCtrl"
+        })
+        .when("/Menu1", {
+            templateUrl: "/Views/Menu1.html",
+          
+        })
+        .when("/Menu2", {
+            templateUrl: "/Views/Menu2.html",
+           
+        })
+        .when("/AboutMe", {
+            templateUrl: "/Views/AboutMe.html",
+           
+        })
+        .when("/Accenture", {
+            templateUrl: "/Views/Accenture.html",
+           
+        })
+        .when("/TCS", {
+            templateUrl: "/Views/TCS.html",
+            
+        })
+    .when("/FreeComputerClass", {
+        templateUrl: "/Views/FreeComputerClass.html",
+        controller: "FreeComputerClassCtrl"
+    })
+        .when("/Contact", {
+            templateUrl: "/Views/Contact.html",
+            controller: "ContactCtrl"
+        })
+         .when("/Login", {
+             templateUrl: "/Views/Login.html",
+
+         })
+        .when("/Register", {
+            templateUrl: "/Views/Register.html",
+
+        })
+    .otherwise({
+        templateUrl: "/Views/Main.html"
+    });
+});
+
+app.controller('CarouselDemoCtrl', function ($scope) {
+    $scope.myInterval = 2000;
+    $scope.noWrapSlides = false;
+    $scope.active = 0;
+    var slides = $scope.slides = [];
+    var currIndex = 0;
+
+    $scope.addSlide = function () {
+        var newWidth = 600 + slides.length + 1;
+        slides.push({
+            image: '//unsplash.it/' + newWidth + '/300',
+            text: ['SharePoint 2013 Developer', 'Im available for work / business', 'Microsoft Azure', 'Cloud / Android Services'][slides.length % 4],
+            id: currIndex++
+        });
+    };
+
+    //$scope.randomize = function () {
+    //    var indexes = generateIndexesArray();
+    //    assignNewIndexesToSlides(indexes);
+    //};
+
+    for (var i = 0; i < 4; i++) {
+        $scope.addSlide();
+    }
+
+    // Randomize logic below
+
+    function assignNewIndexesToSlides(indexes) {
+        for (var i = 0, l = slides.length; i < l; i++) {
+            slides[i].id = indexes.pop();
+        }
+    }
+
+    function generateIndexesArray() {
+        var indexes = [];
+        for (var i = 0; i < currIndex; ++i) {
+            indexes[i] = i;
+        }
+        return shuffle(indexes);
+    }
+
+    //// http://stackoverflow.com/questions/962802#962890
+    //function shuffle(array) {
+    //    var tmp, current, top = array.length;
+
+    //    if (top) {
+    //        while (--top) {
+    //            current = Math.floor(Math.random() * (top + 1));
+    //            tmp = array[current];
+    //            array[current] = array[top];
+    //            array[top] = tmp;
+    //        }
+    //    }
+
+    //    return array;
+    //}
+});
+//app.controller("SampleCtrl", function ($scope, $firebaseObject) {
+
+//    //$scope.name = "I love FoodTruckMumbai";
+   
+//    var ref = firebase.database().ref();
+//        // download the data into a local object
+//    //$scope.name = $firebaseObject(ref);
+//    var syncObject = $firebaseObject(ref);
+//        // synchronize the object with a three-way data binding
+//        // click on `index.html` above to see it used in the DOM!
+//        syncObject.$bindTo($scope, "data");
+   
+
+//});
+// let's create a re-usable factory that generates the $firebaseAuth instance
+app.factory("Auth", ["$firebaseAuth",
+    function ($firebaseAuth) {
+        return $firebaseAuth();
+    }
+]);
+
+
+// and use it in our controller
+app.controller("SampleCtrl", ["$scope", "Auth", "$location", "$firebaseArray",
+    function ($scope, Auth, $location, $firebaseArray) {
+        $scope.auth = Auth;
+        var ref = firebase.database().ref();
+        $scope.messages = $firebaseArray(ref);
+        $scope.auth.$onAuthStateChanged(function (firebaseUser) {
+            $scope.firebaseUser = firebaseUser;
+            if (firebaseUser) {
+                //alert("User auth changed");
+                //$scope.firebaseUser = firebaseUser;
+            } else {
+                //alert("No change");
+            }
+        });
+        var firebaseUser =Auth.$getAuth();
+        if (firebaseUser) {
+            //alert("Signed in as:" + firebaseUser.uid);
+            $scope.firebaseUser = firebaseUser;
+        } else {
+           // alert("No log in");
+        }
+
+       
+        $scope.updateProfileUser = function () {
+       
+            $scope.message = null;
+            var user = firebase.auth().currentUser;
+            user.updateProfile({
+                displayName: $scope.firstName + $scope.lastName
+                
+            }).then(function () {
+                // Update successful.
+                $location.path("/");
+                $scope.$apply();
+                $scope.message = $scope.message + "User created with uid";
+                //$scope.firebaseUser = firebaseUser;
+            }, function (error) {
+                // An error happened.
+            });
+        };
+        $scope.createUser = function () {
+            $scope.dataLoading = true;
+            $scope.message = null;
+            $scope.error = null;
+
+            // Create a new user
+            Auth.$createUserWithEmailAndPassword($scope.email, $scope.password)
+                .then(function (firebaseUser) {
+                    $scope.message = "User created with uid: " + firebaseUser.uid;
+                    $scope.updateProfileUser();
+                }).catch(function (error) {
+                    $scope.error = error;
+                });
+        };
+        $scope.signIN = function () {
+            $scope.dataLoading = true;
+            $scope.message = null;
+            $scope.error = null;
+            // Create a new user
+            Auth.$signInWithEmailAndPassword($scope.email, $scope.password)
+                .then(function (firebaseUser) {
+                    $location.path("/");
+                    $scope.message = "User created with uid: " + firebaseUser.uid + firebaseUser.displayName;
+                    $scope.firebaseUser = firebaseUser;
+                    
+                }).catch(function (error) {
+                    $scope.error = error;
+                });
+            
+        };
+        
+        $scope.SignOutUSer = function () {
+            
+            Auth.$signOut().then(function () {
+                $scope.message = "User sign out";
+            }).catch(function (error) {
+                $scope.error = error;
+            });
+        };
+        $scope.deleteUser = function () {
+            $scope.message = null;
+            $scope.error = null;
+
+            // Delete the currently signed-in user
+            Auth.$deleteUser().then(function () {
+                $scope.message = "User deleted";
+            }).catch(function (error) {
+                $scope.error = error;
+            });
+        };
+        $scope.sendPassReset = function () {
+            Auth.$sendPasswordResetEmail($scope.email).then(function () {
+                console.log("Password reset email sent successfully!");
+            }).catch(function (error) {
+                console.error("Error: ", error);
+            });
+        };
+    }
+]);
+app.controller('NavCtrl',
+    ['$scope', '$location', function ($scope, $location) {
+        $scope.navClass = function (page) {
+            var currentRoute = $location.path().substring(1) || 'home';
+                        return page === currentRoute ? 'active' : '';
+        };
+    }]);
+app.controller("FoodTruckMumbaiCtrl", function ($scope) {
+
+    $scope.msg = "I love FoodTruckMumbai";
+});
+app.controller("FreeComputerClassCtrl", function ($scope) {
+
+    $scope.msg = "I love FreeComputerClass";
+});
+app.controller("ContactCtrl", function ($scope) {
+
+    $scope.msg = "I love Contact";
+});
